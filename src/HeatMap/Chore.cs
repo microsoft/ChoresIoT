4be408dcd93476e13817b58ac154d8233cdab02b
Brady Gaster <bradyg@microsoft.com>
2020-08-21 01:31:00 -0700

    started heat map ui

---
+++ src/HeatMap/Chore.cs
@@ -0,0 +1,11 @@
+namespace HeatMap
+{
+    public class Chore
+    {
+        public string ZoneId { get; set; }
+        public string Message { get; set; }
+        public string AssignedTo { get; set; }
+        public int Status { get; set; }
+        public int Threshold { get; set; }
+    }
+}
